5143243f647ffa318e75cd940a99a8d0c028f9a0
PedroRocha10 <pedromatias130@gmail.com>
2020-05-07 13:01:52 -0300

    site

---
+++ Site - VoltSolar ATUALIZADO/js/js.js
@@ -0,0 +1,43 @@
+    function fecharlogin(){
+        login.style.display = "none"
+        tampa.style.display = "none"
+        finan.style.display = "none"
+        bemvindo.style.display = "none"
+    }
+    function financeiro(){
+        finan.style.display = "block"
+    }
+window.onscroll = function () { fmenu() };
+    window.onscroll = function () { fdesce() };
+    var updown = document.getElementById("menu");
+    var sticky = updown.offsetTop;
+
+    function fmenu() {
+        if (window.pageYOffset > sticky) {
+            updown.classList.add("sticky");
+        }
+        else {
+            updown.classList.remove("sticky");
+        }
+    }
+    function fdesce() {
+        if (window.pageYOffset > sticky) {
+            linkseta.href = "#menu";
+            imgupdown.src = "icones/up.png";
+        }
+        else {
+            linkseta.href = "#rodape";
+            imgupdown.src = "icones/down.png";
+        }
+    }
+    function logar() {
+        login.style.display = "block";
+        corpo.style.position = "fixed";
+        tampa.style.display = "block";
+    }
+    function fechar() {
+        login.style.display = "none";
+        finan.style.display = "none"
+        corpo.style.position = "relative";
+        tampa.style.display = "none";
+    }
